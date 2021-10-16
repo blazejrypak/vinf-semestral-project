78f1aba1963d8ec50d6c72ce8584160a4e524fec
blazejrypak <rypak.b@gmail.com>
2021-10-16 20:52:50 +0200

    file per item

--- news_scraper/news_scraper/pipelines.py
+++ news_scraper/news_scraper/pipelines.py
@@ -5,36 +5,20 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
 from scrapy.exporters import JsonLinesItemExporter
-import datetime
+import uuid
 
 class NewsScraperExporterPipeline(object):
-    """Bulk items exporter"""
 
     def open_spider(self, spider):
-        self.current_exporter = JsonLinesItemExporter(file=self.get_file(), encoding='utf-8', indent=0)
-        self.current_exporter.start_exporting()
-        self.current_exporter_scraped_items = 0
-        self.limit_bulk_items = 2000
-
-    def get_file(self, file_name=None):
-        if file_name:
-            return open('/Users/blazejrypak/Projects/vinf-project/data/' + file_name + '.json', 'wb')
-        now = datetime.datetime.now()
-        return open('/Users/blazejrypak/Projects/vinf-project/data/'+now.strftime("%d-%m-%Y-%H-%M-%S") + '-article.json', 'wb')
+        pass
 
     def close_spider(self, spider):
-        self.current_exporter.finish_exporting()
-
-    def _exporter_for_item(self):
-        if self.current_exporter_scraped_items >= self.limit_bulk_items:
-            self.current_exporter = JsonLinesItemExporter(file=self.get_file(), encoding='utf-8', indent=0)
-            self.current_exporter_scraped_items = 0
-        return self.current_exporter
+        pass
 
     def process_item(self, item, spider):
-        self.current_exporter = self._exporter_for_item()
-        self.current_exporter.export_item(item)
-        self.current_exporter_scraped_items += 1
-        return item
+        json_file = open('/Users/blazejrypak/Projects/vinf-project/collection/'+ str(uuid.uuid4()) + '.json', 'wb')
+        exporter = JsonLinesItemExporter(file=json_file, encoding='utf-8', indent=0)
+        exporter.start_exporting()
+        exporter.export_item(item)
+        exporter.finish_exporting()
